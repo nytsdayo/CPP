78978ece3ce7979781d45f7b49698e71ebab7ab1
nytsdayo <nyantsutsu@gmail.com>
2025-08-20 19:27:19 +0900

    refactoring for ex00 & each Makefile

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,18 +3,19 @@
 #include <cctype>      
 
 int main(int argc, char* argv[]) {
+	std::string output_string;
+
     if (argc > 1) {
 		for (int i = 1; i < argc; i++)
 		{
 			std::string str = argv[i];
-			for (size_t i = 0; i < str.length(); ++i) {
-        		str[i] = std::toupper(static_cast<unsigned char>(str[i]));
+			for (size_t j = 0; j < str.length(); ++j) {
+        		output_string += static_cast<char>(std::toupper(static_cast<unsigned char>(str[j])));
     		}
-	        std::cout << str;
 		}
-		std::cout << std::endl;
     }else{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		output_string = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	}
+	std::cout << output_string << std::endl;
     return 0;
 }
